7359df43f33f0c40c6c726866635be1bc7160493
jonathanec94 <jona_egebak@hotmail.dk>
2016-11-07 12:47:31 +0100

    Project start up

---
+++ src/main/java/dk/cphbusiness/teamperfrontend/testMock.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.cphbusiness.teamperfrontend;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class testMock{
+
+}
