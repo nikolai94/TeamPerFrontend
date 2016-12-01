b20180f374c22f701e35b9223b2913da25ca45e4
Nikolai94 <nikolai@cybermanager.dk>
2016-12-01 12:39:22 +0100

    rest api

--- src/main/java/Rest/Admin.java
+++ src/main/java/Rest/Admin.java
@@ -6,15 +6,34 @@
 package Rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dtos.DepartureDetail;
+import dtos.DepartureIdentifier;
+import dtos.PersonDetail;
+import dtos.RouteIdentifier;
+import dtos.RouteSummary;
+import etos.LoginException;
+import etos.NotFoundException;
+import etos.UnexpectedErrorException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import mock.AdminMock;
 
 /**
  * REST Web Service
@@ -24,6 +43,9 @@ import javax.ws.rs.core.MediaType;
 @Path("admin")
 public class Admin {
 
+    Gson gson;
+    AdminMock admin;
+
     @Context
     private UriInfo context;
 
@@ -31,26 +53,142 @@ public class Admin {
      * Creates a new instance of GenericResource
      */
     public Admin() {
+        gson = new Gson();
+        admin = new AdminMock();
     }
 
-     @GET
-    @Path("test")
+
+    //e.g json {"username" : "user", "password": "password"}
+    @POST
+    @Path("login")
     @Produces("application/json")
-    public String getJson() {
-        ArrayList<Test> list = new ArrayList<Test>();
-        list.add(new Test("a"));
-        list.add(new Test("b"));
-        list.add(new Test("c"));
-        Gson gson = new Gson();
-        return gson.toJson(list);
-    }
+    public String login(String str) {
+        JsonObject jsonObject = gson.fromJson(str, JsonObject.class);
+        String username = jsonObject.get("username").getAsString();
+        String password = jsonObject.get("password").getAsString();
 
-    /**
-     * PUT method for updating or creating an instance of GenericResource
-     * @param content representation for the resource
-     */
+        String toJson = errorMsg("Maybe wrong username or password");
+        try {
+            toJson = gson.toJson(admin.login(username, password));
+        } catch (LoginException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+        return toJson;
+    }
+    
+    //json e.g. = {"routeIdentifier" : {"routeId": "1"}, "localDateTime": "2016-11-30 12:30"}
+    @POST
+    @Path("createDeparture")
+    @Produces("application/json")
+    public String createDeparture(String str) {
+        //!!! Not working in backend dummy
+       
+        /*JsonObject jsonObject = gson.fromJson(str, JsonObject.class);
+        
+        RouteIdentifier routeIdentifier = gson.fromJson(jsonObject.get("routeIdentifier"), RouteIdentifier.class);
+        
+        //Date
+        Locale locale = new Locale("da", "DK");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        formatter = formatter.withLocale(locale);
+        //LocalDate date = LocalDate.parse(jsonObject.get("localDateTime").getAsString(), formatter);
+        LocalDateTime date = LocalDateTime.parse(jsonObject.get("localDateTime").getAsString(), formatter);
+        
+        String toJson = errorMsg("Error");
+        
+        try {
+            toJson = gson.toJson(admin.createDeparture(routeIdentifier, date));
+        } catch (UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+        
+        return toJson;*/
+        
+        return "Not working";
+    }
+    
+    @GET
+    @Path("getAllDepartures")
+    @Produces("application/json")
+    public String getAllDepartures() {
+       
+        String toJson = errorMsg("Error");    
+        try {
+            toJson = gson.toJson(admin.getAllDepartures());
+        } catch (UnexpectedErrorException ex) {
+          ex.printStackTrace();
+        }
+        
+        return toJson;
+    }
+    
+    @GET
+    @Path("getDeparture/{id}")
+    @Produces("application/json")
+    public String getDeparture(@PathParam("id") Long id) {
+        long longId = new Long(id);
+       
+        String toJson = errorMsg("Error");    
+        try {
+            toJson = gson.toJson(admin.getDeparture(longId));
+        } catch (NotFoundException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        } 
+        
+        return toJson;
+    }
+    
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    @Path("updateDeparture")
+    @Produces("application/json")
+    public String updateDeparture(String str) {
+        //DepartureDetail DepartureDetail = gson.fromJson(str, DepartureDetail.class);
+        JsonObject jsonObject = gson.fromJson(str, JsonObject.class);
+        //RouteSummary
+        RouteSummary routeSummary = gson.fromJson(jsonObject.get("routeSummary"), RouteSummary.class);
+       
+        //departureId
+        Long departureId = new Long(jsonObject.get("departureId").getAsString());
+        
+        //Date
+        Locale locale = new Locale("da", "DK");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        formatter = formatter.withLocale(locale);
+        LocalDateTime date = LocalDateTime.parse(jsonObject.get("departureTime").getAsString(), formatter);
+         
+       
+        //DepartureDetail
+        DepartureDetail departureDetail = new DepartureDetail(date, routeSummary, departureId);
+        
+        String toJson = errorMsg("Error");
+        try {
+           toJson = gson.toJson(admin.updateDeparture(departureDetail));
+        } catch (NotFoundException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        } 
+        
+        return toJson;
+    }
+    
+    @DELETE
+    @Path("deleteDeparture/{id}")
+    @Produces("application/json")
+    public String deleteDeparture(@PathParam("id") Long id) {
+        long longId = new Long(id);
+        String toJson = errorMsg("Error");
+        try {
+            toJson = gson.toJson(admin.deleteDeparture(longId));
+        } catch (NotFoundException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        } 
+        return toJson;
+    }
+    
+    
+
+    private static String errorMsg(String errorMsg) {
+        JsonObject innerObject = new JsonObject();
+        innerObject.addProperty("errorMsg", errorMsg);
+        return innerObject.toString();
     }
 }
--- src/main/java/Rest/User.java
+++ src/main/java/Rest/User.java
@@ -6,8 +6,25 @@
 package Rest;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import dtos.PersonDetail;
 import dtos.RouteIdentifier;
+import entities.TravellingEntity;
+import etos.InvalidReservationException;
+import etos.NoAvailableDateException;
+import etos.UnexpectedErrorException;
+import mock.UserMock;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.DateFormatter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -26,8 +43,9 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("user")
 public class User {
-   Gson gson;
-     
+
+    Gson gson;
+    UserMock user;
     @Context
     private UriInfo context;
 
@@ -35,42 +53,107 @@ public class User {
      * Creates a new instance of GenericResource
      */
     public User() {
+
         gson = new Gson();
+        user = new UserMock();
     }
 
-    /*@GET
-    @Path("test")
-    @Produces("application/json")
-    public String getJson() {
-        ArrayList<Test> list = new ArrayList<Test>();
-        list.add(new Test("test"));
-        list.add(new Test("test1"));
-        list.add(new Test("aabbcc"));
-        Gson gson = new Gson();
-        return gson.toJson(list);
-    }*/
-    
     @GET
     @Path("getDepartures/{routeIdentifier}/{departureDate}")
     @Produces("application/json")
-    public String getDepartures(@PathParam("routeIdentifier") Integer routeIdentifier, @PathParam("departureDate") String departureDate) {
-        //return gson.toJson("test");
-        return "{\"routeIdentifier\": \""+ routeIdentifier +"\", \"departureDate\": \""+ departureDate +"\"}";
+    public String getDepartures(@PathParam("routeIdentifier") Integer routeIdentifier, @PathParam("departureDate") String departureDate) throws ParseException {
+
+        //Date
+        Locale locale = new Locale("da", "DK");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        formatter = formatter.withLocale(locale);
+        //LocalDate date = LocalDate.parse("2005-nov-12", formatter);
+        LocalDate date = LocalDate.parse(departureDate, formatter);
+
+        RouteIdentifier route = new RouteIdentifier(routeIdentifier);
+
+        String toJson = null;
+        try {
+            toJson = gson.toJson(user.getDepartures(date, route));
+        } catch (NoAvailableDateException | UnexpectedErrorException e) {
+            e.printStackTrace();
+        }
+        return toJson;
+
+        //return "{\"routeIdentifier\": \""+ routeIdentifier +"\", \"departureDate\": \""+ departureDate +"\"}";
     }
-    
+
+    private static String ErrorMsgInJson() {
+        return null;
+    }
+
     @GET
     @Path("getDeparture/{departureId}")
     @Produces("application/json")
     public String getDeparture(@PathParam("departureId") Integer departureId) {
-        //return gson.toJson("test");
-        return "{\"departureId\": \""+ departureId +"\"}";
+        String toJson = null;
+        try {
+            toJson = gson.toJson(user.getDeparture(departureId));
+        } catch (etos.NotFoundException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+        return toJson;
     }
     
+    @GET
+    @Path("getAllRouteSummaries")
+    @Produces("application/json")
+    public String getAllRouteSummaries() {
+        String toJson = null;
+        try {
+          toJson = gson.toJson(user.getAllRouteSummaries());
+        } catch (UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+        return toJson;
+    }
+
     @POST
     @Path("createReservation")
     @Produces("application/json")
-    public String createReservation(String str){
-       return str; 
+    public String createReservation(String str) {
+        //!!! Not working in backend dummy
+        
+        
+        /*JsonObject jsonObject = gson.fromJson(str, JsonObject.class);
+
+        PersonDetail personDetail = gson.fromJson(jsonObject.get("personDetail"), PersonDetail.class);
+        long departureId = Long.parseLong(jsonObject.get("departureId").toString());
+
+        TravellingEntity[] travellingEntityArray = gson.fromJson(jsonObject.get("travellingEntity"), TravellingEntity[].class);
+        List<TravellingEntity> travellingEntityList = Arrays.asList(travellingEntityArray);
+        try {
+            user.createReservation(personDetail, departureId, travellingEntityList);
+        } catch (InvalidReservationException | UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+
+        return str;*/
+        return "Not working";
     }
     
+    
+    @GET
+    @Path("getTravellingEntities")
+    @Produces("application/json")
+    public String getTravellingEntities() {
+        //!!! Not working in backend dummy
+       
+        /*String toJson = null;
+        try {
+           System.out.println(user.getTravellingEntities().toString());
+           toJson = gson.toJson(user.getTravellingEntities());
+        } catch (UnexpectedErrorException ex) {
+            ex.printStackTrace();
+        }
+        return toJson;*/
+        return "Not working";
+    }
+    
+
 }
